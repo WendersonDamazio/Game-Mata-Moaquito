c32281b98983a8a876db4b14543dcf85bb296a79
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-03-31 20:44:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 let altura = 0 
 let largura = 0 
+let vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -13,8 +14,21 @@ function ajustaTamanhoPalcoJogo(){
 ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
-    var posicaoX = Math.floor(Math.random() * largura) - 90
-    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    //remover o mosquito anterioe (caso exista)
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        if(vidas < 3){
+            
+        }
+        document.getElementById('V' + vidas).src = '../imagens/coracao_vazio.png'
+
+        vidas++
+    }
+
+    var posicaoX = Math.floor(Math.random() * largura) - 180
+    var posicaoY = Math.floor(Math.random() * altura) - 170
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
@@ -24,10 +38,38 @@ function posicaoRandomica() {
     //criar o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio() +' '+ ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio(){
+    let classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio(){
+    let lado = Math.floor(Math.random() * 2)
+
+    switch(lado){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 }
